f8cdbcf2bdaa9e8eff153a5ae328315af0a0d7df
Renata R C Pedreschi <99511265+RenataRCPedreschi@users.noreply.github.com>
2022-04-20 23:10:18 -0300

    "Primeiro commit"

---
+++ porto-tech-store/src/test/java/com/portotechstore/portotechstore/PortoTechStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portotechstore.portotechstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortoTechStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
