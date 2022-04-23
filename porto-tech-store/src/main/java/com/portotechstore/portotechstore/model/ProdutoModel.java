d5627fd03e80accb909b29171508f875a8bd7b2e
heitorgusan <heitorgusan@gmail.com>
2022-04-23 12:29:59 -0300

    model

---
+++ porto-tech-store/src/main/java/com/portotechstore/portotechstore/model/ProdutoModel.java
@@ -0,0 +1,46 @@
+package com.portotechstore.portotechstore.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity //Indica que essa classe será uma entidade
+@Table(name = "produtos") //Nome da tabela no banco de dados
+public class ProdutoModel {
+
+	//Campos da Tabela: ID,NOME,PRECO_VENDA,PRECO_CUSTO,QTDE_ESTOQUE,DATA_CADASTRO
+	
+	//Id do Produto
+	@Id//Chave primária
+	@GeneratedValue(strategy= GenerationType.IDENTITY) //Gerador automático de Ids (Ex: 1,2,3,4,5...)
+	private long id;
+	
+	//Nome Produto
+	@NotNull //Obrigatório
+	@Size(min=3,max=30) // Qtde de caracteres no nome
+	private String nome;
+
+	//Preço de Venda
+	@NotNull //??????????? É apenas isso mesmo?
+	private double preco_venda;
+	
+	//Preço de custo
+	@NotNull //??????????? É apenas isso mesmo?
+	private double preco_custo;
+	
+	//Qtde Estoque
+	@NotNull //??????????? É apenas isso mesmo?
+	private int qtde_estoque;
+	
+	//Data de Cadastro
+	@Temporal(TemporalType.TIMESTAMP) //Informando que o tipo de dado é um tipo date timestamp
+	private Date date = new java.sql.Date(System.currentTimeMillis());//Captura a data exata que o dado  foi inserido
+}
