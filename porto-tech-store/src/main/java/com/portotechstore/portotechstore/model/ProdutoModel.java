fb1ce9961c7d22710602ae0caf40c9ac9e318d93
Rafael Pacheco <rnunespac@gmail.com>
2022-04-24 15:09:54 -0300

    atualizacao do ProdutosModel

--- porto-tech-store/src/main/java/com/portotechstore/portotechstore/model/ProdutoModel.java
+++ porto-tech-store/src/main/java/com/portotechstore/portotechstore/model/ProdutoModel.java
@@ -12,39 +12,38 @@ import javax.persistence.TemporalType;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-@Entity //Indica que essa classe será uma entidade
-@Table(name = "produtos") //Nome da tabela no banco de dados
+@Entity // Indica que essa classe será uma entidade
+@Table(name = "produtos") // Nome da tabela no banco de dados
 public class ProdutoModel {
 
-	//Campos da Tabela: ID,NOME,PRECO_VENDA,PRECO_CUSTO,QTDE_ESTOQUE,DATA_CADASTRO
-	
-	//Id do Produto
-	@Id//Chave primária
-	@GeneratedValue(strategy= GenerationType.IDENTITY) //Gerador automático de Ids (Ex: 1,2,3,4,5...)
+	// Campos da Tabela: ID,NOME,PRECO_VENDA,PRECO_CUSTO,QTDE_ESTOQUE,DATA_CADASTRO
+
+	// Id do Produto
+	@Id // Chave primária
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // Gerador automático de Ids (Ex: 1,2,3,4,5...)
 	private long id;
-	
-	//Nome Produtos
-	@NotNull //Obrigatório
-	@Size(min=3,max=30) // Qtde de caracteres no nome
+
+	// Nome Produtos
+	@NotNull // Obrigatório
+	@Size(min = 3, max = 30) // Qtde de caracteres no nome
 	private String nome;
 
-	//Preço de Venda
-	@NotNull //??????????? É apenas isso mesmo?
+	// Preço de Venda
+	@NotNull // ??????????? É apenas isso mesmo?
 	private double preco_venda;
-	
-	//Preço de custo
-	@NotNull //??????????? É apenas isso mesmo?
+
+	// Preço de custo
+	@NotNull // ??????????? É apenas isso mesmo?
 	private double preco_custo;
-	
-	//Qtde Estoque
-	@NotNull //??????????? É apenas isso mesmo?
+
+	// Qtde Estoque
+	@NotNull // ??????????? É apenas isso mesmo?
 	private int qtde_estoque;
-	
-	//Data de Cadastro
+
+	// Data de Cadastro
 	@Temporal(TemporalType.TIMESTAMP) //Informando que o tipo de dado é um tipo date timestamp
-	private Date date = new java.sql.Date(System.currentTimeMillis());//Captura a data exata que o dado  foi inserido
-<<<<<<< HEAD
-=======
+	private Date date = new java.sql.Date(System.currentTimeMillis());// Captura a data exata que o dado foi inserido
+	
 
 	public long getId() {
 		return id;
@@ -93,7 +92,6 @@ public class ProdutoModel {
 	public void setDate(Date date) {
 		this.date = date;
 	}
+
 	
-	
->>>>>>> 6ae8fa3e198ece40821c1c841f2b780f697e3288
 }
